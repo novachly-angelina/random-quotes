f6081cfebe3e2a36dd90904aa54541e63d742010
Angelina <a.v.novachly@gmail.com>
2026-03-16 13:32:41 +0200

    Splitting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,78 @@
+const quotes = [
+  {
+    quote: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Stay hungry, stay foolish',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Life is what happens when you are busy making other plans',
+    author: 'John Lennon',
+  },
+  {
+    quote: 'In the middle of difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    quote: 'Strive not to be a success, but rather to be of value',
+    author: 'Albert Einstein',
+  },
+  {
+    quote:
+      'The future belongs to those who believe in the beauty of their dreams',
+    author: 'Eleanor Roosevelt',
+  },
+  {
+    quote: 'It does not matter how slowly you go as long as you do not stop',
+    author: 'Confucius',
+  },
+  {
+    quote:
+      'Success usually comes to those who are too busy to be looking for it',
+    author: 'Henry David Thoreau',
+  },
+  {
+    quote: 'Do what you can, with what you have, where you are',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote: 'Believe you can and you are halfway there',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote: 'If you want to lift yourself up, lift up someone else',
+    author: 'Booker T. Washington',
+  },
+  {
+    quote: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    quote: 'Simplicity is the ultimate sophistication',
+    author: 'Leonardo da Vinci',
+  },
+  {
+    quote: 'Whether you think you can or you think you cannot, you are right',
+    author: 'Henry Ford',
+  },
+  {
+    quote: 'The secret of getting ahead is getting started',
+    author: 'Mark Twain',
+  },
+  {
+    quote: 'Everything you have ever wanted is on the other side of fear',
+    author: 'George Addair',
+  },
+];
+
+export default quotes;
